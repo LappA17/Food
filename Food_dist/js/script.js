8e523f08f0a0a71b4ce9033882b2eecfa1d904dc
Ruslan <ruslan.postoyuk@gmail.com>
2022-01-09 11:58:13 +0200

    54. Красивое оповещение пользователя

--- Food_dist/js/script.js
+++ Food_dist/js/script.js
@@ -98,7 +98,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
     const modalTrigger = document.querySelectorAll('[data-modal]'),
         modal = document.querySelector('.modal'),
-        modalCloseBtn = document.querySelector('[data-close]');
+        /*modalCloseBtn = document.querySelector('[data-close]'); - в 54 уроке мы ее удаляем */
 
     modalTrigger.forEach(btn => {
         btn.addEventListener('click', openModal);
@@ -117,12 +117,12 @@ window.addEventListener('DOMContentLoaded', function() {
         clearInterval(modalTimerId);
     }
     
-    modalCloseBtn.addEventListener('click', closeModal);
+    //modalCloseBtn.addEventListener('click', closeModal); эту тоже удаляем 
 
     modal.addEventListener('click', (e) => {
-        if (e.target === modal) {
-            closeModal();
-        }
+        if (e.target === modal || e.target.getAttribute("data-close" == "") {
+            closeModal(); 
+        } // или е-таргет будет этим крестиком непосредственно , благодаря тому что мы можем получить этот атрбиту благодаря команде гетАтрибут и в него поместим data-close, и нам нужно поместить в него пустую строку потому что это крестик. И теперь даже в том модельном окне что мы только что создали крестик тоже будет работать
     });
 
     document.addEventListener('keydown', (e) => {
@@ -131,8 +131,8 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     });
 
-    // const modalTimerId = setTimeout(openModal, 3000);
-    // Закомментировал, чтобы не отвлекало
+    const modalTimerId = setTimeout(openModal, 50000);
+    // Изменил значение, чтобы не отвлекало
 
     function showModalByScroll() {
         if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
@@ -210,80 +210,90 @@ window.addEventListener('DOMContentLoaded', function() {
         'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
         21,
         ".menu .container"
-    ).render(); 
+    ).render();
 
     // Forms
 
-    const forms = document.querySelectorAll("from"); //начнем с получения всех форм которые у нас есть на странице по тегу form
-
-    const message = { // этот объект будет содержать список всех фраз которыя я хочу показывать в разных ситуациях
-        loadind: "загрузка",
-        success: "спасибо, скоро свяжимся",
-        failure: "ошибка"
+    const forms = document.querySelectorAll('form');
+    const message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так...'
     };
 
-   forms.forEach(item =>{ //нужно взять все наши формы и уже под каждую из них подвязать функцию postData
-    postData(item);
-   }); 
+    forms.forEach(item => {
+        postData(item);
+    });
 
-    function postData(form) { // эта функция будет принимать в себя какуе-то form и эта функция будет отвечать за постинг данных. Мы передаем какуе-то форму потому что нам на эту форму будет легко просто взять и навесить обработчик событий
-        form.addEventListener("submit", (e) => {// submit срабатывает каждый раз когда мы пытаемся отправить какуе-то форму. На сайте есть кнопка перезвоните мне, и когда мы заполняем данные и отправляет то сабмит срабатывает каждый раз как это происходит и отправляет данные нам на сервер.
-
-        /* добавляем e потому что нам нужно отключить стандартное поведение браузера, потому что каждый раз как мы отправляет форму страничка перезагружается*/
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
             e.preventDefault();
 
-            const statusMessage = document.createElement ("div"); //создаем переменую с новым элментом на странице для того что бы реализовать новосозданную переменую message 
-            statusMessage.classList.add("status");
+            let statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.appendChild(statusMessage);
+        
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+            const formData = new FormData(form);
 
-            statusMessage.textContent = message.loadind; //  во внутрь нашей переменной благодаря textContet помещаем сообщение которое мы хотим показать пользователю в ответ на его действие. Самое первое это загрузка потому что как только он что-то сделал происходит сначала загрузка, он увидет это смс только в случае очень медленого интернета
-
-            form.append(statusMessage); //теперь очень важно отправить это сообщение statusMessage куда-то на страницу ведь пока что он существует только в javascript
-
-            const request = new XMLHttpRequest();//помни что всегд опен вызывается что бы настроитьXMLHttpRequest 
-            request.open("POST", "server.php");// сервер.пхп - путь на который мы будем ссылаться
-            
-            //request.setRequestHeader("Content-type", "multipart/form-data"); //сначала ваня сделал тот код чо ниже (формДата) и потом пошел делать вот эти заголовки
-            /* !!! сейчас когда мы используем связку XMLHttpRequest + FormData, то нам заголовок устанавливат не нужно - он устанавливается автоматически. ЭТО ОЧЕНЬ ВАЖНЫЙ НЮАНС !!!  */
-
-            // ТЕПЕРЬ РАЗБЕРЕМ ЕСЛИ ДАННЫЕ НУЖНО ПЕРЕДАВАТЬ В ФОРМАТЕ JSON (Это Ваня в конце видео показал как в формате не FormData а JSON данные передавать)
-
-        request.setRequestHeader("Content-type", "application/json"); // это прийдется уже расскомментировать
-
-            const formData = new FormData(form);// ОЧЕНЬ ВАЖНО new FormData - это такой же тип данных как JSON ! переменная formData будет в себе помещать все данные отправленные пользователем (тоже ввиду формат, ключ, значение). В new FormData мы помещаем форму из котороя нам нужно собрать данные, то-есть (form). 
-
-            const object = {}; // что бы поменять данные FormData на JSON мы создаем новый объект
-            formData.forEach(function(value, key) { //метод forEach переберет все в form что бы потом поместить в object.
-                object[key] = value; //то-есь на основание тех данных которые были в formData мы мы сформулируем object при помощи обычного перебора
+            const object = {};
+            formData.forEach(function(value, key){
+                object[key] = value;
             });
+            const json = JSON.stringify(object);
 
-            const json = JSON.stringify(object);//этот метод привращает обычный объект в json. И ТЕПЕРЬ ВСЕ ЧТО НА НУЖНО СДЕЛАТЬ ЭТО ВЗЯТЬ json  и пометсить reques.send(json) вместо форм дата
+            request.send(json);
 
-
-            /* ЗДЕСЬ ЕСТЬ ОЧЕНЬ ВАЖНЫЙ МОМЕНТ КОТОРЫЙ ЗАВИСИТ ОТ ВЕРСТАЛЬЩИКА И КАК СВЕРСТАНЫ ИНПУТЫ(ЛИБО ОПШИНЫ ЛИБО ТЕКСТ ЕРЕА) - ТО ЕСТЬ ТЕ ДАННЫЕ КОТОРЫЕ БУДУТ ИДТИ НА СЕРВЕР, МЫ ДОЛЖНЫ АБСОЛЮТНО ВСЕГДА ЗАДАВАТЬ АТРИБУТ name (и name не должны повторяться) */
-
-            request.send(formData); // отправляем форм дата которая была основана на основание того всего что мы написали 
-
-            request.addEventListener("load", () => { //мы отслеживаем load - конечную загрузку нашего запроса
-                if (request.status === 200 ) {
-                    console.log(request.response); // нам нужно пользователя проинформировать что все хорошо, ты все правильно сделал, идеальный пример это как с на нашем сайте т500 после того как пользователь оставил свои данные - ему приходила инфа наш менеджер с вами свяжиться. По этому мы сейчас вверху создаем новую переменую message
-
-                    statusMessage.textContent = message.success;// если все хорошо то будет сюксес и помещаем мы это в условие где request.status === 200 - очень хорошо
-
-                    form.reset();//форма очиститься, то-есть имя и номер телефона что мы указали просто пропадут после того как пользователь оставил заявку
-
-                    setTimeout(() => {
+            request.addEventListener('load', () => {
+                if (request.status === 200) {
+                    console.log(request.response);
+                    /*statusMessage.textContent*/showThanksModal(message.success) = message.success;
+                    form.reset();
+                    statusMessage.remove();
+                    /*setTimeout(() => {
                         statusMessage.remove();
-                    }, 2000);//это что бы надписа спасибо мы с вами свяжимся пропала через 2 секунды
-
+                    }, 2000); мы освобождаем от таймаута потому что у нас statusMessage будет использоваться только для loading. И теперь для statusMessage будет использоваться уже модельное окно
+                    По этому мы удаляем  statusMessage.textContent и там где он был мы начнем вызывать нашу новую созданную функию */
                 } else {
-                    statusMessage.textContent = message.failure;
+                    /*statusMessage.textContent*/ showThanksModal(message.failure); //то-есть теперь мы будем не statusMessage модифицировать а помещаем сообщение в данную функцию 
                 }
-            }); 
-
-        }); 
-
-
-
+            });
+        });
     }
-    // CMND + SHIFT + R = СБРОСС КЕША, ПОСЛЕ КАЖДЫХ ИЗМЕНЕНИЙ НА СЕРВЕРЕ, ВСЁ НОВОЕ ЧТО Я СДЕЛАЛ ДОЛЖНО ПОДГРУЗИТЬСЯ ПОСЛЕ СБРОССА КЕША
+
+    // теперь будем создавать модельное окно как в т500 thanks , что тебя перебрасывает на мод окно с спасибо за покупку наш менедежр отзвонит, оно еще thanks называлось 
+
+    function showThanksModal(message){ // смс о статусе отправки будем передавать как аргумент message и добавим его в новосозданное модельное окно и на самом деле мы будем брать этот аргумент из объекта создного выше 
+
+        const prevModalDialog = document.querySelector(".modal__dialog"); // у нас на страничке уже есть модельное окно, когда мы до этого делали свяжитесь со мной, и нам нужно его сейчас получить что бы после того как пользователь оставлял заявку это модельное окно заменялось другим. Важно что мы модельное окно для пользователя старое не удаляем а просто скрываем
+
+        prevModalDialog.classList.add("hide"); // таким образом с помощью класса hide мы можем скрыть модельное окно
+        openModal(); //будет открывать новое модельное окно
+
+        const thanksModal = document.createElement("div");
+        thanksModal.classList.add("modal__dialog");// добавляем к новому окну тот же класс что бы мы один модел дайлег заменяли другим
+
+        thanksModal.innerHTML = `
+        <div class="modal__content">
+            <div class="modal__close" data-close>×</div>
+            <div class="modal__title">${message}</div>
+        </div>`;
+/*modal__close - это класс где находится крестик закрытия окна , я его скопировал прям с html .
+ОЧЕНЬ ВАЖНО а тот тут запутанно всё, этот крести там где дата клоуз был создан не автоматически благодаря html а динамически , вручную мной, я его вставил с верстки, по этому он функционально работать не будет, но тут мы вспоминаем что у нас есть функция закрытия модельного окна modalCloseBtn которую мы делали намного раньше (код в строке 101 и 120) 
+
+теперь рас уж мы создали этот элемент внутри джаваскрипта нам нужно поместить его на страницу*/
+
+document.querySelector(".modal").append(thanksModal); //  теперь наш элемент появится на странице, но если все так оставить то форма просто удалится, нам нужно сделать так что бы клиент всегда мог видеть форму на сайте для повторной отправки
+
+setTimeout(() => {
+    thanksModal.remove();
+    prevModalDialog.classList.add("show");
+    prevModalDialog.classList.remove("hide");
+    closeModal();// из-за того что у пользователся начинается удалятся наша модалка и показываться предыдущий контент то мы просто должны закрыть модельное окно
+}, 4000);
+    
+};
 });
